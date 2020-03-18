661440eafc64fac3fca8b67ba9af9206850e904f
Eugene Yu <youzhi.yu@gmail.com>
2020-03-18 10:21:54 +0800

    Add Cloud Functions code for bucket to bucket sync

---
+++ cloud-functions/bucket_to_bucket.py
@@ -0,0 +1,47 @@
+from google.cloud import storage
+from google.cloud.exceptions import NotFound
+import os
+
+BUCKET_DEST = os.environ['BUCKET_DEST']
+
+def copy_object(event, context):
+
+    bucket_name = event['bucket']
+    object_name_src = event['name']
+    object_name_dest = object_name_src
+
+    storage_client = storage.Client()
+    source_bucket = storage_client.bucket(bucket_name)
+    source_blob = source_bucket.blob(object_name_src)
+    destination_bucket = storage_client.bucket(BUCKET_DEST)
+
+    blob_copy = source_bucket.copy_blob(
+        source_blob, destination_bucket, object_name_dest
+    )
+
+    print(
+        "Blob {} in bucket {} copied to blob {} in bucket {}.".format(
+            source_blob.name,
+            source_bucket.name,
+            blob_copy.name,
+            destination_bucket.name,
+        )
+    )
+
+def delete_object(event, context):
+    bucket_name = event['bucket']
+    object_name_src = event['name']
+
+    storage_client = storage.Client()
+
+    destination_bucket = storage_client.bucket(BUCKET_DEST)
+
+    try:
+        destination_bucket.delete_blob(object_name_src)
+    except NotFound:
+        print("Sync deletion of object {} from bucket {} to bucket {} Not Found".format(
+                object_name_src,
+                bucket_name,
+                destination_bucket.name,
+            )
+        )
